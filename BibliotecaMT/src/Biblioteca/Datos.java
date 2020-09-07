8a261f34eeee6aa09ad0de5a46b6f5d24a83136a
MatiRasnik <MatiRasnik@users.noreply.github.com>
2020-09-07 19:00:00 -0300

    datos

--- BibliotecaMT/src/Biblioteca/Datos.java
+++ BibliotecaMT/src/Biblioteca/Datos.java
@@ -2,43 +2,14 @@ package Biblioteca;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.swing.JFrame;
 
 public class Datos extends JFrame {
 
-	public static void AgregarUsuario() {
-		String usuario;
-		String contra;
-		int tipo;
-		
-		//parte grafica
-		
-		try {
-		ConexionBD conexion = new ConexionBD();
-        Connection con = conexion.conectarConBase(); 
-        String buscarAdmin = "select * from usuario";
-        Statement cs =  con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-		ResultSet resultado = cs.executeQuery(buscarAdmin);
-		while (resultado.next())
-		{
-			String Usuario = resultado.getString("usuario");
-			if(usuario == Usuario ) {
-					//ya existe un usuario con ese nombre
-			}else {
-				 String insertString = "INSERT INTO usuarios" + "(id,usuario,contra,tipo) values" + "('usuario', 'contra', 'tipo')";
-				 //el usuario se a registrado correctamnete 
-			}
-		}	
-		} catch (ClassNotFoundException  e) {
-			e.printStackTrace();
-			System.out.println("Error en la conexion con el Driver");
-			} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Error en la conexion con la Base");
-			}
-	}
+
 	public static void AgregarLibro() {
 		
 		int tipo;
@@ -85,10 +56,7 @@ public class Datos extends JFrame {
 					}
 				}
 			}	
-		}} catch (ClassNotFoundException  e) {
-			e.printStackTrace();
-			System.out.println("Error en la conexion con el Driver");
-			} catch (SQLException e) {
+		}} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Error en la conexion con la Base");
 
